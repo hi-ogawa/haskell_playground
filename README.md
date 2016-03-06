39f22e388a4a7eb6a45c4944602697f8b3f37392
hiogawa <hi.ogawa.zz@gmail.com>
2016-03-06 15:41:36 +0900

    set ghc 7.10 + update README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 [![Build Status](https://travis-ci.org/hi-ogawa/haskell_playground.png?branch=master)](https://travis-ci.org/hi-ogawa/haskell_playground)
+
+
+__References__
+
+- input output is taken from uDebug: https://www.udebug.com/
--- src/Uva/P10065.hs
+++ src/Uva/P10065.hs
@@ -14,14 +14,7 @@ module Uva.P10065 where
 
 import Control.Exception (assert)
 
--- NOTE:
---   - on my local ghc 7.10, (<*) and (<$>) loaded via GHC.Base.
---   - on travis ci ghc 7.8, they are not.
-import Control.Applicative ((<*))
-import Data.Functor ((<$>))
-
-import Control.Monad.Writer
-
+import Control.Monad.Writer (Writer, execWriter, tell, lift)
 import Text.Parsec
 import Numeric (showFFloat)
 
