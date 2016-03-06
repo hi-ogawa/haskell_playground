1908460afb74458106022abab3770ec44d19691a
hiogawa <hi.ogawa.zz@gmail.com>
2016-03-06 13:44:46 +0900

    fix excecutable + add usage

--- src/Main.hs
+++ src/Main.hs
@@ -6,12 +6,21 @@ import Control.Exception (assert)
 import qualified UVA10054 as UVA10054
 import qualified Uva.P10065 as P10065
 
+-- example usage:
+--
+--   # build executable
+--   $ cabal build
+--
+--   # see output
+--   $ ./dist/build/main/main 10065 < ./resources/UVA10065.input
+--
+--   # directly check diff using bash syntax: http://askubuntu.com/questions/229447/how-do-i-diff-the-output-of-two-commands
+--   $ diff <(./dist/build/main/main 10065 < ./resources/UVA10065.input) <(cat ./resources/UVA10065.output)
+
 main :: IO ()
 main =
   do args <- getFullArgs
      case head args of
-       "10054" -> P10065.main
-       "10065" -> UVA10054.main
+       "10054" -> UVA10054.main
+       "10065" -> P10065.main
        _ -> assert False undefined
-
-
