186e09a367971019db66d2c0daaefb1ec3400441
hiogawa <hi.ogawa.zz@gmail.com>
2016-03-23 09:59:29 +0900

    implement perceptron algorithm (with animation plotting python program)

---
+++ resources/Ml/Perceptron/plot.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+xs = np.linspace(-1, 1, 1000)
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
+
+c0, c1 = open("points.dat").read().strip().split("\n\n")
+ls = open("lines.dat").read().strip().split("\n")
+
+def parse_points(lines):
+    ls = lines.strip().split("\n")
+    xys = list(map(lambda l: list(map(lambda s: float(s), l.split(" "))), ls))
+    return (list(map(lambda xy: xy[0], xys)), list(map(lambda xy: xy[1], xys)))
+
+
+def parse_line(line):
+    a, b, c = line.strip().split(" ")
+    return (float(a), float(b), float(c))
+
+c0_xs, c0_ys = parse_points(c0)
+c1_xs, c1_ys = parse_points(c1)
+
+abcs = list(map(parse_line, ls))
+
+fig = plt.figure()
+plt.plot(c0_xs, c0_ys, 'b+', c1_xs, c1_ys, 'ro')
+
+def yline(x, a, b, c):
+    if b != 0:
+        return - (a/b)*x - c/b
+    else:
+        raise BaseException
+
+def update_line(num, data, line):
+    a, b, c = data[num]
+    line.set_data(xs, np.vectorize(lambda x: yline(x, a, b, c))(xs))
+    return line,
+
+l, = plt.plot([], [])
+plot_ani = animation.FuncAnimation(fig, update_line, len(abcs), fargs=(abcs, l),
+                                   interval=100, blit=False)
+
+plot_ani.save("plot.mp4")
---
+++ src/Ml/Perceptron.hs
@@ -0,0 +1,117 @@
+module Ml.Perceptron where
+
+import System.Random
+import Data.List (partition, intercalate)
+import Numeric (showFFloat)
+import Control.Monad.Writer
+
+-- TODO:
+--   - implement two versions
+--     - offline
+--     - online
+--   - convergence theorem of linearly seperable instances and time complexity
+--   - gradient descent terminology:
+--     - learning rate - step size (must be 0..1 ?)
+--     - standard - offline - batch
+--     - online - stochatic (mathematical derivation of this method)
+
+
+------------------------
+-- Problem Definition --
+
+-- GIVEN:
+--   - class a: {..., x_i, ...}  \subseteq X
+--   - class b: {..., x'_i, ...} \subseteq X'
+-- FIND linear function f: R^2 -> R
+-- s.t.
+--   for all x  \in X.  f(x)  >= 0        -- (a)
+--   for all x' \in X'. f(x') <  0        -- (a')
+
+-- notes:
+--   - we deal with only the special case where
+--     - input is two dimentional
+--     - 2 categories classification
+--   - represent linear function f as coefficients (w0, w1) and bias constant (w2):
+--      f : (u, v) |-> w0*u + w1*v + w2
+--   - we employ normalization for calculation simplicity: e.g.
+--      - extend (u, v) to (u, v, 1) to deal with `w2` nicely
+--      - negate the elements in the second class X'
+
+type R_2 = (Double, Double)
+type R_3 = (Double, Double, Double)
+
+
+---------------
+-- algorithm --
+
+rate, eps :: Double
+rate = 0.001
+eps = 0.00001
+
+-- returns each parameters of gradient descent steps
+offline :: [R_2] -> [R_2] -> [R_3]
+offline xs xs' = offlineNormalized (nXs ++ nXs')
+  where
+    nXs  = map (\(u, v) -> (u, v, 1)) xs
+    nXs' = map (\(u, v) -> (-u, -v, -1)) xs'
+
+offlineNormalized :: [R_3] -> [R_3]
+offlineNormalized xs = execWriter $ loop (0.1, 0.1, 0.1)
+  where
+    loop :: R_3 -> Writer [R_3] ()
+    loop p | _E p <= eps = tell [p] >> return ()
+           | otherwise = tell [p] >> loop (p `minus` (rate `times` grad_E p))
+    _E :: R_3 -> Double
+    _E p = sum . map (negate . (p `dot`)) . filter ((0 >) . (p `dot`)) $ xs
+    grad_E :: R_3 -> R_3
+    grad_E p = (dE0 p, dE1 p, dE2 p)
+    dE0, dE1, dE2 :: R_3 -> Double
+    dE0 p = sum . map (negate . \(u, _, _) -> u) . filter ((0 >) . (p `dot`)) $ xs
+    dE1 p = sum . map (negate . \(_, v, _) -> v) . filter ((0 >) . (p `dot`)) $ xs
+    dE2 p = sum . map (negate . \(_, _, s) -> s) . filter ((0 >) . (p `dot`)) $ xs
+
+
+-- vector calculation
+
+dot :: R_3 -> R_3 -> Double
+dot (w0, w1, w2) (u, v, s) = w0*u + w1*v + w2*s
+
+minus :: R_3 -> R_3 -> R_3
+minus (w0, w1, w2) (w0', w1', w2') = (w0 - w0', w1 - w1', w2 - w2')
+
+times :: Double -> R_3 -> R_3
+times c (w0, w1, w2) = (c*w0, c*w1, c*w2)
+
+----------
+-- main --
+
+main :: IO ()
+main = do
+  writeFile "resources/Ml/Perceptron/points.dat" (formatSamples samples)
+  writeFile "resources/Ml/Perceptron/lines.dat" (formatLines $ uncurry offline samples)
+
+
+-- linearly separable samples
+
+samples :: ([R_2], [R_2])
+samples = partition (\(u, v) -> lineFunc u >= v) . take n $ xs
+  where
+    n = 100
+    g = mkStdGen 0
+    xs :: [R_2]
+    xs = fix (\f ls -> (ls !! 0, ls !! 1):(f (drop 2 ls))) $ randomRs (-1, 1) g
+    lineFunc u = 0.8 * u + 0.4
+
+formatSamples :: ([R_2], [R_2]) -> String
+formatSamples samples = dat
+  where
+    dat = format (fst samples) ++ "\n\n" ++ format (snd samples)
+    format :: [R_2] -> String
+    format = unlines . map (\(u, v) -> showFloat u ++ " " ++ showFloat v)
+
+formatLines :: [R_3] -> String
+formatLines ls =
+  unlines . map (\(x, y, z) -> intercalate " " . map showFloat $ [x, y, z]) $ ls
+
+showFloat :: Double -> String
+showFloat d = ($ "") $ showFFloat (Just 3) d
