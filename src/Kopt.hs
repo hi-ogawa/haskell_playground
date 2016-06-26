acb2d04c1f1c3cc84895674e75a510e6588c10f8
hiogawa <hi.ogawa.zz@gmail.com>
2016-06-26 13:45:33 +0900

    refactor threeOpt with replacing mutable array with immutable array

--- src/Kopt.hs
+++ src/Kopt.hs
@@ -1,71 +1,54 @@
 {-# LANGUAGE TupleSections #-}
 module Kopt (threeOpt, spec) where
 
--- import Control.Arrow ((>>>))
-import Control.Monad
-import Data.Array.IO -- TODO: using mutable array for swap operation doesn't make sense
 import qualified Data.Vector as V
 import Data.Function (fix)
-import Data.Maybe (listToMaybe)
+import Data.List (find)
 
 import Test.Hspec
 
 -- assume complete graph is represented as adjacency matrix
 type Matrix = V.Vector (V.Vector Double)
-type Path = IOUArray Int Int
+type Path = V.Vector Int
 
 -- four pattern of 3 edge swapping
 data Pattern = A | B | C | D
              deriving (Show, Eq)
 
-threeOpt :: Matrix -> IO [Int]
+threeOpt :: Matrix -> [Int]
 threeOpt mtrx = do
   let numVs = V.length mtrx
       trpls = triples numVs
       dbls = doubles numVs
-  path <- newListArray (0, numVs) ([0..(numVs - 1)] ++ [0]) :: IO Path
-  fix $ \loop -> do
-    mybTrpl <- findM (goodTriple mtrx path) trpls
-    case mybTrpl of
-      Just trpl -> swapTriple path trpl >> loop
-      Nothing -> do
-        mybDbl <- findM (goodDouble mtrx path) dbls
-        case mybDbl of
-          Just dbl -> swapDouble path dbl >> loop
-          Nothing -> return ()
-  getElems path
+      initPath = V.fromList $ [0..(numVs - 1)] ++ [0]
+  V.toList $ (`fix` initPath) $ \loop path ->
+    let mybTrpl = find (goodTriple mtrx path) trpls
+        mybDbl  = find (goodDouble mtrx path) dbls in
+    case (mybTrpl, mybDbl) of
+      (Just trpl, _) -> loop $ swapTriple path trpl
+      (_, Just dbl)  -> loop $ swapDouble path dbl
+      _              -> path
 
-weight :: Matrix -> Path -> Int -> Int -> IO Double
-weight mtrx path pathIdxOrig pathIdxDest = do
-  vOrig <- readArray path pathIdxOrig
-  vDest <- readArray path pathIdxDest
-  return $ mtrx V.! vOrig V.! vDest
+weight :: Matrix -> Path -> Int -> Int -> Double
+weight mtrx path i i' = mtrx V.! (path V.! i) V.! (path V.! i')
 
-goodTriple :: Matrix -> Path -> (Int, Int, Int, Pattern) -> IO Bool
-goodTriple mtrx path trpl@(i, j, k, _) = do
-  prevWeight <- sum <$> mapM (uncurry (weight mtrx path)) [(i, i+1), (j, j+1), (k, k+1)]
-  nextWeight <- sum <$> mapM (uncurry (weight mtrx path)) (tripleEdgesIdcs trpl)
-  return $ nextWeight < prevWeight
+goodTriple :: Matrix -> Path -> (Int, Int, Int, Pattern) -> Bool
+goodTriple mtrx path trpl@(i, j, k, _) =
+  let decr = sum $ map (uncurry (weight mtrx path)) [(i, i+1), (j, j+1), (k, k+1)]
+      incr = sum $ map (uncurry (weight mtrx path)) (tripleEdgesIdcs trpl) in
+  incr < decr
 
-goodDouble :: Matrix -> Path -> (Int, Int) -> IO Bool
-goodDouble mtrx path dbl@(i, j) = do
-  prevWeight <- sum <$> mapM (uncurry (weight mtrx path)) [(i, i+1), (j, j+1)]
-  nextWeight <- sum <$> mapM (uncurry (weight mtrx path)) (doubleEdgesIdcs dbl)
-  return $ nextWeight < prevWeight
+goodDouble :: Matrix -> Path -> (Int, Int) -> Bool
+goodDouble mtrx path dbl@(i, j) =
+  let decr = sum $ map (uncurry (weight mtrx path)) [(i, i+1), (j, j+1)]
+      incr = sum $ map (uncurry (weight mtrx path)) (doubleEdgesIdcs dbl) in
+  incr < decr
 
-swapTriple :: Path -> (Int, Int, Int, Pattern) -> IO ()
-swapTriple path trpl =
-  ($ triplePathIdcs trpl) $
-    mapM (readArray path) >=>
-    return . zip [0..] >=>
-    mapM_ (\(i, v) -> writeArray path i v)
+swapTriple :: Path -> (Int, Int, Int, Pattern) -> Path
+swapTriple path trpl = V.fromList $ map (path V.!) $ triplePathIdcs (length path) trpl
 
-swapDouble :: Path -> (Int, Int) -> IO ()
-swapDouble path dbl =
-  ($ doublePathIdcs dbl) $
-    mapM (readArray path) >=>
-    return . zip [0..] >=>
-    mapM_ (\(i, v) -> writeArray path i v)
+swapDouble :: Path -> (Int, Int) -> Path
+swapDouble path dbl = V.fromList $ map (path V.!) $ doublePathIdcs (length path) dbl
 
 -- [] if n < 5
 triples :: Int -> [(Int, Int, Int, Pattern)]
@@ -90,22 +73,19 @@ tripleEdgesIdcs (i, j, k, p) =
     C -> [(i, j+1), (i+1, k+1), (k,   j  )]
     D -> [(i, k  ), (k+1, j+1), (j,   k+1)]
 
-triplePathIdcs :: (Int, Int, Int, Pattern) -> [Int]
-triplePathIdcs (i, j, k, p) =
+triplePathIdcs :: Int -> (Int, Int, Int, Pattern) -> [Int]
+triplePathIdcs n (i, j, k, p) =
   case p of
-    A -> [i, j] ++ (reverse [i+1..j-1]) ++ (reverse [j+1..k]) ++ [k+1]
-    B -> [i] ++ [j+1..k] ++ [i+1..j] ++ [k+1]
-    C -> [i] ++ [j+1..k] ++ (reverse [i+1..j]) ++ [k+1]
-    D -> [i] ++ (reverse [j+1..k]) ++ [i+1..j] ++ [k+1]
+    A -> [0..i-1] ++ [i, j] ++ (reverse [i+1..j-1]) ++ (reverse [j+1..k]) ++ [k+1..n-1]
+    B -> [0..i-1] ++ [i] ++ [j+1..k] ++ [i+1..j]                          ++ [k+1..n-1]
+    C -> [0..i-1] ++ [i] ++ [j+1..k] ++ (reverse [i+1..j])                ++ [k+1..n-1]
+    D -> [0..i-1] ++ [i] ++ (reverse [j+1..k]) ++ [i+1..j]                ++ [k+1..n-1]
 
 doubleEdgesIdcs :: (Int, Int) -> [(Int, Int)]
 doubleEdgesIdcs (i, j) = [(i, j), (i+1, j+1)]
 
-doublePathIdcs :: (Int, Int) -> [Int]
-doublePathIdcs (i, j) = [i] ++ (reverse [i+1..j]) ++ [j+1]
-
-findM :: Monad m => (a -> m Bool) -> [a] -> m (Maybe a)
-findM f ls = listToMaybe <$> filterM f ls
+doublePathIdcs :: Int -> (Int, Int) -> [Int]
+doublePathIdcs n (i, j) = [0..i] ++ (reverse [i+1..j]) ++ [j+1..n-1]
 
 
 -------------
@@ -130,4 +110,4 @@ spec = do
                 , (8, 4)
                 , (9, 0)
                 ]
-      threeOpt (mkAdjMatrix pts) >>= (`shouldBe` [0,1,4,6,7,8,5,3,2,0])
+      threeOpt (mkAdjMatrix pts) `shouldBe` [0,1,4,6,7,8,5,3,2,0]
