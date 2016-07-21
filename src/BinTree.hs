be00a259ea98046fde3eb433186ba3ff51ca8d8d
hiogawa <hi.ogawa.zz@gmail.com>
2016-07-21 09:55:47 +0900

    add BinTree.insertWith/deleteWith/neighborsWith

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -45,8 +45,56 @@ viewMin t = f t >>= \x -> return (x, delete x t)
     f (N x L _) = Just x
     f (N _ t' _) = f t'
 
+
+-- BentleyOttmann y-structure interface
+
+insertWith :: Ord b => (a -> b) -> a -> BinTree a -> BinTree a
+insertWith _ x L = N x L L
+insertWith f x (N x' t0 t1) | f x <= f x' = N x' (insertWith f x t0) t1
+                             | otherwise = N x' t0 (insertWith f x t1)
+
+deleteWith :: Ord b => (a -> b) -> a -> BinTree a -> Maybe (BinTree a)
+deleteWith _ _ L = Nothing
+deleteWith f x (N x' t0 t1) | f x < f x' = N x' <$> deleteWith f x t0 <*> pure t1
+                             | f x > f x' = N x' t0 <$> deleteWith f x t1
+                             | f x == f x' = Just $ merge f t0 t1
+                             | otherwise = undefined
+  where
+    merge :: Ord b => (a -> b) -> BinTree a -> BinTree a -> BinTree a
+    merge _ t L = t
+    merge _ L t = t
+    merge _f t (N _x _t0 _t1) = N _x t $ merge _f _t0 _t1
+
+neighborsWith :: Ord b => (a -> b) -> a -> BinTree a -> (Maybe a, Maybe a)
+neighborsWith _f _x _t = g _f _x _t (Nothing, Nothing)
+  where
+    g :: Ord b => (a -> b) -> a -> BinTree a -> (Maybe a, Maybe a) -> (Maybe a, Maybe a)
+    g _ _ L _ = (Nothing, Nothing)
+    g f x (N x' t0 _) (m, _) | f x < f x'  = g f x t0 (m, Just x')
+    g f x (N x' _ t1) (_, m) | f x > f x'  = g f x t1 (Just x', m)
+    g f x (N x' t0 t1) mm    | f x == f x' =
+      case (t0, t1, mm) of
+        (L, L, _     ) -> mm
+        (_, L, (_, m)) -> (rightMost t0, m)
+        (L, _, (m, _)) -> (m, leftMost t1)
+        (_, _ , _    ) -> (rightMost t0, leftMost t1)
+    g _ _ _ _ = undefined
+
+leftMost :: BinTree a -> Maybe a
+leftMost L = Nothing
+leftMost (N x L _) = Just x
+leftMost (N _ t _) = leftMost t
+
+rightMost :: BinTree a -> Maybe a
+rightMost L = Nothing
+rightMost (N x _ L) = Just x
+rightMost (N _ _ t) = rightMost t
+
+
+-- testing --
+
 spec :: Spec
-spec =
+spec = do
   describe "fromList, insert, delete" $
     it "." $ do
       let t = fromList [3, 5, 2, 6 :: Int]
@@ -57,3 +105,15 @@ spec =
                                      (N 6 L L))
       delete 5 t `shouldBe` (N 3 (N 2 L L)
                                  (N 6 L L))
+  describe "neighborsWith" $
+    it "." $ do
+      let t = fromList [3, 5, 2, 6, 4 :: Int]
+      t `shouldBe` N 3 (N 2 L L)
+                       (N 5 (N 4 L L)
+                            (N 6 L L))
+      neighborsWith id 2 t `shouldBe` (Nothing, Just 3)
+      neighborsWith id 3 t `shouldBe` (Just 2, Just 4)
+      neighborsWith id 4 t `shouldBe` (Just 3, Just 5)
+      neighborsWith id 5 t `shouldBe` (Just 4, Just 6)
+      neighborsWith id 6 t `shouldBe` (Just 5, Nothing)
+      neighborsWith id 8 t `shouldBe` (Nothing, Nothing)
